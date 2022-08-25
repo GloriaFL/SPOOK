1b7451fbce456261090ae847c5389a84188f6dd1
Daniel Ramiro Lanz <danielramiro91@hotmail.com>
2022-08-25 14:41:21 +0200

    fixed a few back end and footer problems, the footer ones are provisional

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,6 @@ class Booking < ApplicationRecord
   has_many :reviews
 
   validates :number_people, presence: true, numericality: { only_integer: true }
-  validates :status, presence: true
   validates :date_start, :date_end, presence: true
   validates :date_end, comparison: { greater_than: :date_start, other_than: Date.today }
 end
