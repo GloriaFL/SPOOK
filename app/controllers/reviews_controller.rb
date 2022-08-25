a6370f218193bd26cb1f137f2edfbf607a26f140
gloria font <enlagloria26@gmail.com>
2022-08-25 14:50:26 +0200

    commiting to pull

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,15 @@
 class ReviewsController < ApplicationController
   def create
-    @destination = Destination.find(params[:destination_id])
+    @booking = Booking.fing(params[:booking_id])
     @review = Review.new(review_params)
     @review.destination = @destination
     if @review.save
-      redirect_to destination_path(@destination)
-    else
-      render "destinations/show", status: :unprocessable_entity
     end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
